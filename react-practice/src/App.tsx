ef440bc4ff37320616ab4f2ae4bf3872b6491f97
MikkisGuy <69403498+MikkisGuy@users.noreply.github.com>
2021-04-10 01:16:28 +0300

    Added status toggle and task delete

--- react-practice/src/App.tsx
+++ react-practice/src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
 import Todo from './components/Todo';
@@ -15,18 +16,44 @@ interface IProps {
 }
 
 export default function App(props: IProps): JSX.Element {
+  const [tasks, setTasks] = useState(props.tasks);
+
   function addTask(name: string) {
-    alert(name);
+    const newTask = { id: nanoid(), name: name, completed: false };
+
+    setTasks([...tasks, newTask]);
   }
 
-  const taskList = props.tasks.map((task) => (
+  function toggleTaskCompleted(id: string) {
+    const updatedTasks = tasks.map((task) => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed };
+      }
+
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  function deleteTask(id: string) {
+    const remainingTasks = tasks.filter((task) => id !== task.id);
+
+    setTasks(remainingTasks);
+  }
+
+  const taskList = tasks.map((tasks) => (
     <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
+      id={tasks.id}
+      name={tasks.name}
+      completed={tasks.completed}
+      key={tasks.id}
+      onCheckboxChange={toggleTaskCompleted}
+      onDelete={deleteTask}
     />
   ));
+
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
   return (
     <div className="todoapp stack-large">
@@ -37,7 +64,7 @@ export default function App(props: IProps): JSX.Element {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul className="todo-list stack-large stack-exception">{taskList}</ul>
     </div>
   );
--- react-practice/src/components/Todo.tsx
+++ react-practice/src/components/Todo.tsx
@@ -4,13 +4,20 @@ interface IProps {
   name: string;
   completed: boolean;
   id: string;
+  onCheckboxChange: (id: string) => void;
+  onDelete: (id: string) => void;
 }
 
 export default function Todo(props: IProps) {
   return (
     <li className="todo stack-small">
       <div className="c-cb">
-        <input id="todo-0" type="checkbox" defaultChecked={props.completed} />
+        <input
+          id="todo-0"
+          type="checkbox"
+          defaultChecked={props.completed}
+          onChange={() => props.onCheckboxChange(props.id)}
+        />
         <label className="todo-label" htmlFor="todo-0">
           {props.name}
         </label>
@@ -19,7 +26,11 @@ export default function Todo(props: IProps) {
         <button type="button" className="btn">
           Edit <span className="visually-hidden">{props.name}</span>
         </button>
-        <button type="button" className="btn btn__danger">
+        <button
+          type="button"
+          className="btn btn__danger"
+          onClick={() => props.onDelete(props.id)}
+        >
           Delete <span className="visually-hidden">{props.name}</span>
         </button>
       </div>
